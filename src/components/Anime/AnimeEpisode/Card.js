64a124cfb3b0dace62b291473448c6f6355799d8
betakurnia <betakurnia1903@gmail.com>
2022-12-04 04:15:52 +0700

    Separate server and client

---
+++ src/components/Anime/AnimeEpisode/Card.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+import dateFormat from "dateformat"; 
+
+
+const Card = (props) => {
+    const { title, episode, date,  imageEpisode } = props;
+
+  return (
+    <>
+    <Link
+      to={`/${title
+        .toLowerCase()
+        .split(" ")
+        .join("-")}/episode-${episode}`}
+    >
+      <a
+        href="/#"
+        className=" text-light-black utility_text-decoration_underline  anime-header__title_text_18px"
+      >
+        <h4
+          className={`text-uppercase d-flex align-items-end`}
+        >
+          {title}
+        </h4>
+        <h5>Episode {episode}</h5>
+      </a>
+    </Link>
+    <p className="text-light-gray utility_text_14px">
+      {" "}
+      {dateFormat(date, "dddd,h:MM TT ")}
+    </p>
+    <Link
+      to={`/${title
+        .toLowerCase()
+        .split(" ")
+        .join("-")}/episode-${episode}`}
+    >
+      <img
+        src={`./image/episode-anime/${imageEpisode}`}
+        alt={imageEpisode}
+        className="img-fluid"
+      />
+    </Link>
+  </>
+  )
+}
+
+export default Card
